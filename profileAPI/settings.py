226b717a2d3170c85119533cbb3413ca2c7c8342
lumpy <lumpy72006@gmail.com>
2025-10-19 15:20:01 +0100

    set Debug to True

--- profileAPI/settings.py
+++ profileAPI/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", '.pxxl.click', "127.0.0.1"]
 
